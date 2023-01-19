e6656bd9be12a0083e472fde28f9c840993e248e
edgar096 <edgar.goncalves96@gmail.com>
2023-01-19 22:48:38 +0000

    Fixed "Add Todo" Logic. App now renders the new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
 import { useState } from 'react';
 import './App.css';
-const todos = [
-  {
-    id: 43243232,
-    creationdate: '20220114',
-    task: 'Do the dishes',
-    completed: true,
-  },
-  {
-    id: 43243231,
-    creationdate: '20220114',
-    task: 'Do the laundry',
-    completed: true,
-  },
-  {
-    id: 43243233,
-    creationdate: '20220113',
-    task: 'Clean the PC',
-    completed: true,
-  },
-  {
-    id: 43243238,
-    creationdate: '20220112',
-    task: 'Pull the plug',
-    completed: false,
-  },
-];
+import { TodoList } from 'TodoItem';
+import { TodoForm } from 'TodoForm';
 
 function TodoItem({ item }) {
   const [status, setStatus] = useState(item.completed);
@@ -50,31 +26,68 @@ function TodoList({ data }) {
   return data.map((item) => <TodoItem item={item} key={item.id} />);
 }
 
-function App() {
+function TodoForm({ handleAddTodo }) {
   const [task, setTask] = useState('');
-  const [todoList, setTodoList] = useState(todos);
 
   function handleNewTodo(e) {
     e.preventDefault();
-    setTodoList([
-      { id: Math.random(), task: task, completed: false },
-      ...todoList,
-    ]);
+    handleAddTodo(e.target.formreact.value);
   }
   function handleNewTodoTask(e) {
     setTask(e.target.value);
   }
 
   return (
+    <form onSubmit={handleNewTodo}>
+      <label>
+        Task:
+        <input type="text" onChange={handleNewTodoTask} name="formreact" />
+      </label>
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
+
+function App() {
+  let todos = [
+    {
+      id: 43243232,
+      creationdate: '20220114',
+      task: 'Do the dishes',
+      completed: true,
+    },
+    {
+      id: 43243231,
+      creationdate: '20220114',
+      task: 'Do the laundry',
+      completed: true,
+    },
+    {
+      id: 43243233,
+      creationdate: '20220113',
+      task: 'Clean the PC',
+      completed: true,
+    },
+    {
+      id: 43243238,
+      creationdate: '20220112',
+      task: 'Pull the plug',
+      completed: false,
+    },
+  ];
+
+  const [todoList, setTodoList] = useState(todos);
+
+  function handleAddTodo(task) {
+    setTodoList((prev) => [
+      ...prev,
+      { id: Math.random(), task: task, completed: false },
+    ]);
+  }
+  return (
     <>
       <h1>My List of Todos...</h1>
-      <form onSubmit={handleNewTodo}>
-        <label>
-          Task:
-          <input type="text" onChange={handleNewTodoTask} />
-        </label>
-        <input type="submit" value="Add" />
-      </form>
+      <TodoForm handleAddTodo={handleAddTodo} />
       <TodoList data={todoList} />
     </>
   );
